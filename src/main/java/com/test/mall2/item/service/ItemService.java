5ca136fc2fb2b43ea6b16969f1aef07819275cd9
smart-202-15 <smart-202-15@smart-202-15-PC>
2018-04-13 10:21:32 +0900

    [이인호]서블릿 transactional 추가

--- src/main/java/com/test/mall2/item/service/ItemService.java
+++ src/main/java/com/test/mall2/item/service/ItemService.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@Transactional
 public class ItemService {
 
 	@Autowired
 	private ItemDao itemDao;
-	
+		
 	public int insertItem(Item item) {
 		int row = itemDao.insertItem(item);
 		return row;
